884f7e2915ff7c049a528c292c41192e65170a9c
DaisukeInoue <ksthtgtgsndyk@gmail.com>
2018-02-01 22:18:10 +0900

    [update] SalatInjectorTest

--- src/main/java/com/salat/ui/config/SalatInjector.java
+++ src/main/java/com/salat/ui/config/SalatInjector.java
@@ -13,21 +13,25 @@ import com.google.inject.name.Names;
 public class SalatInjector extends AbstractModule {
 
     /** プロパティ名 */
-    private final String PROPERTY_NAME = "salat.properties";
+    private final String PROPERTY_NAME = "config/common/salat.properties";
 
     /** 設定メソッド */
     @Override
     protected void configure() {
-
         // 設定ファイルバインド
+        bindProperties();
+
+        // サーブレット・フィルタ登録
+        install(new SalatModule());
+    }
+
+    /** 設定ファイルバインド */
+    public void bindProperties() {
         try (InputStream inputStream = currentThread().getContextClassLoader().getResourceAsStream(PROPERTY_NAME);) {
             Names.bindProperties(binder(), loadProperties(inputStream));
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
-
-        // サーブレット・フィルタ登録
-        install(new SalatModule());
     }
 
     /**
@@ -38,7 +42,7 @@ public class SalatInjector extends AbstractModule {
      * @return Properties
      * @throws IOException
      */
-    private Properties loadProperties(InputStream inputStream) throws IOException {
+    public Properties loadProperties(InputStream inputStream) throws IOException {
         Properties properties = new Properties();
         if (inputStream != null) {
             properties.load(inputStream);
--- src/test/java/com/salat/ui/config/SalatInjectorTest.java
+++ src/test/java/com/salat/ui/config/SalatInjectorTest.java
@@ -1,59 +1,146 @@
 /** 設定パッケージ */
 package com.salat.ui.config;
 
-import static org.mockito.MockitoAnnotations.initMocks;
+import static org.junit.Assert.fail;
+import java.io.IOException;
 import java.util.Properties;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import com.google.inject.AbstractModule;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.name.Names;
-import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
 import mockit.Mocked;
+import mockit.Tested;
 import mockit.integration.junit4.JMockit;
 
 /** SalatInjectorをテストするクラス */
 @RunWith(JMockit.class)
 public class SalatInjectorTest {
-    @InjectMocks
-    private SalatInjector si;
-    @Mocked
-    private Binder binder;
 
+    @Tested
+    private SalatInjector si;
+
+    @Mocked
+    Binder mockBinder;
+
+    /**
+     * 事前準備
+     */
     @Before
     public void setUp() {
-        initMocks(this);
+        si = new SalatInjector();
     }
 
+    /**
+     * 設定メソッドテスト001_正常系
+     */
+    @SuppressWarnings("unused")
     @Test
-    public void test() {
-        new Expectations() {
-            {
-                Names.bindProperties(binder, new Properties());
-                minTimes = 0;
-            }
-        };
+    public void testConfigure001() {
         new MockUp<AbstractModule>() {
             @Mock
             protected void install(Module module) {
+                // 空実装
             }
 
             @Mock
             protected Binder binder() {
-                return binder;
+                return mockBinder;
+            }
+        };
+
+        new MockUp<Names>() {
+            @Mock
+            void bindProperties(Binder binder, Properties properties) {
+                // 空実装
             }
         };
         si.configure();
     }
 
+    /**
+     * 設定ファイルバインドメソッドテスト001_正常系
+     */
+    @SuppressWarnings("unused")
+    @Test
+    public void testBindProperties001() {
+        new MockUp<AbstractModule>() {
+            @Mock
+            protected void install(Module module) {
+                // 空実装
+            }
+
+            @Mock
+            protected Binder binder() {
+                return mockBinder;
+            }
+        };
+
+        new MockUp<Names>() {
+            @Mock
+            void bindProperties(Binder binder, Properties properties) {
+                // 空実装
+            }
+        };
+        si.bindProperties();
+    }
+
+    /**
+     * 設定ファイルバインドメソッドテスト002_異常系
+     */
+    @SuppressWarnings("unused")
+    @Test
+    public void testBindProperties002() {
+        new MockUp<AbstractModule>() {
+            @Mock
+            protected Binder binder() {
+                return mockBinder;
+            }
+        };
+
+        new MockUp<Names>() {
+            @Mock
+            void bindProperties(Binder binder, Properties properties) throws IOException {
+                throw new IOException();
+            }
+        };
+        si.bindProperties();
+    }
+
+    /**
+     * プロパティ読込メソッドテスト001_正常系
+     */
+    @Test
+    public void testLoadProperties001() {
+        try {
+            si.loadProperties(Thread.currentThread().getContextClassLoader().getResourceAsStream("salat.properties"));
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * プロパティ読込メソッドテスト002_異常系
+     */
+    @Test
+    public void testLoadProperties002() {
+        try {
+            si.loadProperties(null);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * 事後準備
+     */
     @After
     public void tearDown() {
-
+        // 空実装
     }
 }
